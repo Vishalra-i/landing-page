df7c6ad64485a4249f255a73910b357c6573e493
Vishalra-i <vishalrai0392@gmail.com>
2024-07-22 11:37:41 +0530

    fix auto scroll

--- src/components/Smoothscroll.jsx
+++ src/components/Smoothscroll.jsx
@@ -10,25 +10,12 @@ function SmoothScroll() {
         if (targetElement) {
           setTimeout(() => {
             targetElement.scrollIntoView({ behavior: 'smooth' });
-          }, 2000); 
+          }, 2000); // 2 seconds delay
         }
       }
     };
 
     document.addEventListener('click', handleAnchorClick);
-
-    const sections = document.querySelectorAll('section');
-    let currentSectionIndex = 0;
-
-    const autoScroll = () => {
-      if (currentSectionIndex < sections.length) {
-        sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
-        currentSectionIndex++;
-        setTimeout(autoScroll, 3000); // 3 seconds delay between automatic scrolls
-      }
-    };
-
-    setTimeout(autoScroll, 2000); // Initial delay before starting auto-scroll
 
     return () => {
       document.removeEventListener('click', handleAnchorClick);
